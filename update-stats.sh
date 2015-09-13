a56dce3b30c75201a174e2f2c7eeae71d5af2f2b
k-vinogradov <kostya.vinogradov@gmail.com>
2015-09-13 11:20:11 +0700

    Once again

--- update-stats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if systemctl status named >> /dev/null ; then
-    rm -Rf /tmp/named-stats
-    rm -f /var/named/data/named_stats.txt
-    /sbin/rndc stats
-    /bin/python /root/bind-stats-parser/authparser.py -f /var/named/data/named_stats.txt
-fi
